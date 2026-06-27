2a131193d0aba4c07af4aff45297a40608dccbb2
enny-1235 <ennyolvrp2@gmail.com>
2026-06-26 21:37:06 -0300

    Add age check for driving eligibility
    questão da carteira de motorista.

---
+++ idade.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite sua idade: "))
+habilitacao = input("Possui habilitação? (sim/não): ")
+
+if idade >= 18 and habilitacao == "sim":
+    print("Pode dirigir")
+else:
+    print("Não pode dirigir")
